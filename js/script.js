6f4b5fa89207607b5be234f82e4525c02aee21bc
Philipp Steinemann <philipp.steinemann@bl.ch>
2023-12-27 17:13:38 +0100

    auswahl beruf

---
+++ js/script.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $.ajax({
+        url: 'https://sandbox.gibm.ch/berufe.php',
+        method: 'GET',
+        dataType: 'json',
+        success: function(data) {
+            let dropdown = $('#dropdownBeruf');
+
+            $.each(data, function (key, entry) {
+                if(entry.beruf_name) {
+                    dropdown.append($('<option></option>').attr('value', entry.beruf_id).text(entry.beruf_name));
+                }
+            })
+        },
+        error: function() {
+            alert('Es gab ein Problem beim Laden der Daten.');
+        }
+    });
+});
--- readme.md
+++ readme.md
@@ -1,2 +1,13 @@
 # BBZBL Stundenplan
 _Dies ist ein Stundenplan für die BBZBL als Projekt im Modul 133_
+
+
+Weiterführende Links:
+Stundenplan auf BBZBL:
+https://www.bbzbl.ch/stundenplan/
+
+Datumsberechnung:
+https://momentjs.com/
+
+Kalender:
+https://fullcalendar.io/
