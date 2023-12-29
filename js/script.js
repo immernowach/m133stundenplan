0d8d557ccfd12a5f44ca6442105f22b58d3aaa20
Philipp Steinemann <philipp.steinemann@bl.ch>
2023-12-29 14:56:46 +0100

    stundenplan

--- js/script.js
+++ js/script.js
@@ -25,10 +25,12 @@ $(document).ready(function(){
             success: function(data) {
                 let dropdown = $('#dropdownKlasse');
                 dropdown.empty();
+                dropdown.append($('<option></option>').attr('value', '').text('Bitte wählen...')).prop('disabled', true);
 
                 $.each(data, function (key, entry) {
                     if(entry.klasse_name) {
                         dropdown.append($('<option></option>').attr('value', entry.klasse_id).text(entry.klasse_name));
+                        dropdown.prop('disabled', false);
                     }
                 })
             },
@@ -37,5 +39,53 @@ $(document).ready(function(){
             }
         });
     });
+
+
+    $('#dropdownKlasse').change(function() {
+        let classId = $(this).val();
+        if(classId) {
+            let view = calendar.getView();
+            let weekNumber = moment(view.intervalStart).format('ww-YYYY');
+            updateCalendarWithClassSchedule(classId, weekNumber);
+        } else {
+            $('#calendar').fullCalendar('removeEvents');
+        }
+    });
+
+    let calendar = new FullCalendar.Calendar($('#calendar')[0], {
+        initialView: 'dayGridWeek'
+    });
+    calendar.render();
+
+    function updateCalendarWithClassSchedule(classId, weekNumber) {
+        $.ajax({
+            url: 'https://sandbox.gibm.ch/tafel.php',
+            method: 'GET',
+            data: {
+                klasse_id: classId,
+                woche: weekNumber
+            },
+            dataType: 'json',
+            success: function(data) {
+                console.log(classId, data);
+                calendar.removeAllEvents();
+                data.forEach(function(entry) {
+                    calendar.addEvent({
+                        title: entry.tafel_longfach,
+                        start: entry.tafel_datum + 'T' + entry.tafel_von,
+                        end: entry.tafel_datum + 'T' + entry.tafel_bis,
+                        extendedProps: {
+                            lehrer: entry.tafel_lehrer,
+                            raum: entry.tafel_raum,
+                            kommentar: entry.tafel_kommentar
+                        }
+                    });
+                });
+            },
+            error: function() {
+                alert('Es gab ein Problem beim Laden der Stundentafel.');
+            }
+        });
+    }
 
 });
